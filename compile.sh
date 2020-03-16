51c2a41c4a4858cd0a963c4faeca87b2692f9852
oscarigrexas <oscarigglez@gmail.com>
2020-03-16 08:49:48 +0100

    Update

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+echo "Compiling eomt!"
+
+pdflatex eomt # Compile template
+makeindex eomt.nlo -s nomencl.ist -o eomt.nls # Compile nomenclature
+makeindex eomt # Compile index
+biber eomt # Compile bibliography
+makeglossaries eomt # Compile glossary
+pdflatex eomt # Compile template again
+pdflatex eomt # Compile template again
