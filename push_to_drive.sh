fc67ea0cadf8664de55ac4121e04183acd15aa16
Óscar <oscarigrexas@gmail.com>
2020-06-28 11:42:34 +0200

    Update

--- push_to_drive.sh
+++ push_to_drive.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp eomt.pdf /Users/oscar/Google\ Drive/TFM/.
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o eomt-book.pdf images/front-cover.pdf images/front-cover-back.pdf eomt.pdf images/back-cover-back.pdf images/back-cover.pdf
+cp eomt-book.pdf /Users/oscar/Google\ Drive/TFM/.
 cp slides/eomt-slides.pdf /Users/oscar/Google\ Drive/TFM/. 
